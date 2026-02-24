5d11f1b0a0f5b1bb031f56f08dd06595843f0687
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-02-24 20:10:37 +0530

    introduction

--- introduction.py
+++ introduction.py
@@ -5,3 +5,4 @@ print("python is a high-level programming language \n used  to create software a
 print("Importent of PYTHON :-")
 print("* python is object oriented programing")
 print("* easy to understanding. \n * easy to implement \n *  no need of libaray class etc,. \n * it is faster then other language \m * wedlly used in ai \n * it have flexible memory allocation ")
+print(" * the code is exquted in line by line \n")
