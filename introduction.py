c1eeb358e0046d7f8ea145b7298890e33042d62f
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-02-24 18:12:15 +0530

    what is python

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#simple print statement 
+print("hi i am santhu")
+print("what is python?")
+print("python is a high-level programming language \n used  to create software apllication ")
+print("Importent of PYTHON :-")
+print("* python is object oriented programing")
+print("* easy to understanding. \n * easy to implement \n *  no need of libaray class etc,. \n * it is faster then other language \m * wedlly used in ai \n * it have flexible memory allocation ")
--- print_statement.py
+++
@@ -1,4 +0,0 @@
-#simple print statement 
-print("hi i am santhu")
-print("what is python?")
-print()
