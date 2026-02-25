9d0e10ecc8f176a46927570603ef0c05625468f2
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-02-25 23:15:09 +0530

    working

---
+++ print_input_stament.py
@@ -0,0 +1,36 @@
+# Print Statement
+
+print("Print Statement")
+print("This statement is used to display the output on the screen in Python.")
+print("Syntax:")
+print("print()\n")
+
+# Example 1
+print("Hello World\n")
+
+# Quotes for declaring strings
+print("Print statement can be declared using three types of quotes:\n")
+
+# Examples
+print('Single quote')
+print("Double quote")  # Standard quote used in Python
+print('''Triple quote''')
+
+# Using semicolon to write multiple print statements in one line
+print("Hi"); print("My name is"); print("Coder")
+
+print("_____________________________________________________________\n")
+
+# Input Statement
+print("Input Statement:")
+print("This statement is used to take input from the user.")
+print("Syntax:")
+print("variable_name = input('message')\n")
+
+# Example 1
+name = input("Enter your name: ")
+print("Hello, good morning everyone. My name is", name)
+
+# Example 2
+dream = input("What is your dream: ")
+print("Okay, stop dreaming and start working for your dream:", dream)
