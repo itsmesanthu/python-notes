b4a6bc7a7c1f85235a93b09994a7d327ff7fe509
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-02-26 22:56:45 +0530

    updating..

---
+++ variable.py
@@ -0,0 +1,71 @@
+# ==============================
+# VARIABLES IN PYTHON
+# ==============================
+
+print("WHAT IS VARIABLE:\n")
+print("Variable is a name used to store data or value in memory.\n")
+
+# Example
+name = "Santhu"
+age = 21
+print("Example of variable:")
+print(name)
+print(age)
+
+print("\n==============================")
+print("RULES OF VARIABLE:\n")
+
+print("1. Variable must start with a letter or underscore (_)")
+print("   Example: name, _age")
+
+print("\n2. Variable cannot start with number")
+print("   Wrong: 1name")
+
+print("\n3. No spaces allowed in variable name")
+print("   Wrong: my name")
+print("   Correct: my_name")
+
+print("\n4. No special symbols allowed (@ # $ %)")
+print("   Wrong: name@123")
+
+print("\n5. Python is case-sensitive")
+print("   name and Name are different")
+
+print("\n6. Cannot use Python keywords")
+print("   Example: if, for, while, print")
+
+print("\n==============================")
+print("TYPES OF VARIABLES:\n")
+
+# 1. String variable
+print("1. String Variable (stores text)")
+name = "Santhu"
+print(name)
+
+# 2. Integer variable
+print("\n2. Integer Variable (stores numbers)")
+age = 21
+print(age)
+
+# 3. Float variable
+print("\n3. Float Variable (stores decimal values)")
+height = 5.6
+print(height)
+
+# 4. Boolean variable
+print("\n4. Boolean Variable (True/False)")
+is_student = True
+print(is_student)
+
+print("\n==============================")
+print("EXAMPLE PROGRAM:\n")
+
+name = "Santhu"
+age = 21
+height = 5.6
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
