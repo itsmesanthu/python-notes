c051e1ce573af7de3e89c7c4b30a017a3c0afa37
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-03-02 23:32:00 +0530

    makeing

---
+++ type_convertion.py
@@ -0,0 +1,5 @@
+#================================
+#type casting
+#================================
+print("type casting")
+print("type casting is the process of converting one data type into another data type")
